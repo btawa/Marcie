7c621ca4cd1ae1240879402615781a049125a095
Japnix <brendan.tawa@gmail.com>
2019-04-17 21:41:07 -0400

    Adding fftcg_parser

---
+++ fftcg_parser.py
@@ -0,0 +1,226 @@
+import json
+import re
+from html.parser import HTMLParser
+import sys
+import urllib.request
+from urllib.error import URLError, HTTPError
+
+
+# Helpful jq
+#
+# jq '.cards[] | select(.Code == "1-028R")'  ./cards.json
+#
+# JSON Keys
+#
+# Code		Power		Ex_Burst	Text		Text_EN		Text_DE		Text_ES		Text_FR		Text_IT		Type_NA
+# Element	Category_1	Name		Name_EN		Name_DE		Name_ES		Name_FR		Name_IT		Set		Name_NA
+# Rarity		Category_2	Type		Type_EN		Type_DE		Type_ES		Type_FR		Type_IT		Text_NA
+# Cost		Multicard	Job		Job_EN		Job_DE		Job_ES		Job_FR		Job_IT		Job_NA
+
+# This function reads in a request, and a cards dictionary.
+# Based on the request it will search the dictionary for a match
+# based on the FFTCG card code and return a single card.
+
+def grab_card(req, cards):
+    our_card = ''
+
+    for x in cards:
+        if re.search(req, x[u'Code'].upper()):
+            our_card = x
+    return our_card
+
+
+# This function reads in a request, and a cards dictionary.
+# Based on the request it will search for a match based on the
+# Name_EN value in the cards dictionary.  This function can
+# return a single card or multiple cards.
+
+def grab_cards(req, cards):
+    our_cards = []
+
+    for x in cards:
+        if re.search(req.lower(), x[u'Name_EN'].lower()):
+            our_cards.append(x)
+    return our_cards
+
+
+# This reads in a card likely from grab_card or grab_cards and
+# Takes the input formats it to get find and replace unusual unicode
+# characters.  Once this is complete it creates a formatted string.
+# In marciebot this is used specifically for the tinycode function.
+
+def prettyCode(card):
+    # Multicard Check
+    if card[u'Multicard'] == u"\u25cb":
+        multicard = '(Multi)'
+    else:
+        multicard = ''
+
+    # Determine what the element is and convert character to word
+    if card[u'Element'] == u"\u571F":
+        element = "Earth"
+    elif card[u'Element'] == u"\u6c34":
+        element = "Water"
+    elif card[u'Element'] == u"\u706b":
+        element = "Fire"
+    elif card[u'Element'] == u"\u98a8":
+        element = "Wind"
+    elif card[u'Element'] == u"\u6c37":
+        element = "Ice"
+    elif card[u'Element'] == u"\u5149":
+        element = "Light"
+    elif card[u'Element'] == u"\u95c7":
+        element = "Dark"
+    elif card[u'Element'] == u"\u96f7":
+        element = "Lightning"
+
+    line1 = card[u'Code'] + ' - ' + element + " " + card[u'Cost'] + " - " + card[u'Name_EN'] + " - " + card[
+        u'Type_EN'] + " " + multicard
+
+    return line1
+
+
+# This reads in a card likely from grab_card or grab_cards and
+# Takes the input formats it to get find and replace unusual unicode
+# characters.  Once this is complete it creates a formatted string.
+# In marciebot this is used specifically for the name and code functions.
+
+def prettyCard(card):
+    # Determine what the element is and convert character to word
+    if card[u'Element'] == u"\u571F":
+        element = "Earth"
+    elif card[u'Element'] == u"\u6c34":
+        element = "Water"
+    elif card[u'Element'] == u"\u706b":
+        element = "Fire"
+    elif card[u'Element'] == u"\u98a8":
+        element = "Wind"
+    elif card[u'Element'] == u"\u6c37":
+        element = "Ice"
+    elif card[u'Element'] == u"\u5149":
+        element = "Light"
+    elif card[u'Element'] == u"\u95c7":
+        element = "Dark"
+    elif card[u'Element'] == u"\u96f7":
+        element = "Lightning"
+
+    if card[u'Multicard'] == u"\u25cb":
+        multicard = '(Multi)'
+    else:
+        multicard = ''
+
+    #  Prepping different lines for return
+    line1 = element + " " + card[u'Cost'] + " " + card[u'Name_EN'] + " " + "(" + card[u'Code'] + ") " + multicard
+    line2 = card[u'Type_EN'] + " " + card[u'Job_EN'] + " " + card[u'Category_1']  # + " " + card[u'Category_2']
+    line3 = card[u'Text_EN']
+    line4 = card[u'Power']
+
+    # Modify return string based on whether card has power or not
+    if card[u'Power'] == "":
+        finished_string = line1 + "\n" + line2 + "\n" + line3
+    else:
+        finished_string = line1 + "\n" + line2 + "\n" + line3 + "\n" + line4
+
+    # Replace EX Burst
+    finished_string = finished_string.replace('[[ex]]', '')
+    finished_string = finished_string.replace('[[/]]', '')
+    finished_string = finished_string.replace('EX BURST', '[EX BURST]')
+
+    # Formatting Fixes
+    finished_string = finished_string.replace('[[', '<')
+    finished_string = finished_string.replace(']]', '>')
+    finished_string = finished_string.replace('<s>', '')
+    finished_string = finished_string.replace('</>', '')
+    finished_string = finished_string.replace('<i>', '')
+    finished_string = finished_string.replace('<br> ', '\n')
+    finished_string = finished_string.replace('<br>', '\n')
+
+    # Replace Logos
+    finished_string = finished_string.replace(u"\u571F", '(Earth)')
+    finished_string = finished_string.replace(u"\u6c34", '(Water)')
+    finished_string = finished_string.replace(u"\u706b", '(Fire)')
+    finished_string = finished_string.replace(u"\u98a8", '(Wind)')
+    finished_string = finished_string.replace(u"\u6c37", '(Ice)')
+    finished_string = finished_string.replace(u"\u5149", '(Light)')
+    finished_string = finished_string.replace(u"\u95c7", '(Dark)')
+    finished_string = finished_string.replace(u"\u96f7", '(Lightning)')
+
+    # Replace Fullwidth Numbers with normal numbers
+    finished_string = finished_string.replace(u"\uFF11", '(1)')
+    finished_string = finished_string.replace(u"\uFF12", '(2)')
+    finished_string = finished_string.replace(u"\uFF13", '(3)')
+    finished_string = finished_string.replace(u"\uFF14", '(4)')
+    finished_string = finished_string.replace(u"\uFF15", '(5)')
+    finished_string = finished_string.replace(u"\uFF16", '(6)')
+    finished_string = finished_string.replace(u"\uFF17", '(7)')
+    finished_string = finished_string.replace(u"\uFF18", '(8)')
+    finished_string = finished_string.replace(u"\uFF19", '(9)')
+    finished_string = finished_string.replace(u"\uFF10", '(0)')
+
+    # Non full width
+    finished_string = finished_string.replace(u"\u300a"u"\u0031"u"\u300b", '(1)')
+    finished_string = finished_string.replace(u"\u300a"u"\u0032"u"\u300b", '(2)')
+    finished_string = finished_string.replace(u"\u300a"u"\u0033"u"\u300b", '(3)')
+    finished_string = finished_string.replace(u"\u300a"u"\u0034"u"\u300b", '(4)')
+    finished_string = finished_string.replace(u"\u300a"u"\u0035"u"\u300b", '(5)')
+    finished_string = finished_string.replace(u"\u300a"u"\u0036"u"\u300b", '(6)')
+    finished_string = finished_string.replace(u"\u300a"u"\u0037"u"\u300b", '(7)')
+    finished_string = finished_string.replace(u"\u300a"u"\u0038"u"\u300b", '(8)')
+    finished_string = finished_string.replace(u"\u300a"u"\u0039"u"\u300b", '(9)')
+    finished_string = finished_string.replace(u"\u300a"u"\u0030"u"\u300b", '(0)')
+    finished_string = finished_string.replace(u"\u300a" + "X" + u"\u300b", '(X)')
+
+    # Special Switch
+    finished_string = finished_string.replace(u"\u300a"u"\u0053"u"\u300b", '[Special]')
+
+    # Tap Symbol
+    finished_string = finished_string.replace(u"\u30C0"u"\u30EB", '(Dull)')
+
+    # Weird bracket removal:
+    finished_string = finished_string.replace(u"\u300a", '')
+    finished_string = finished_string.replace(u"\u300b", '')
+
+    # Double quotes with YURI?
+    finished_string = finished_string.replace("\"\"", '\"')
+
+    # HTML Parser - Converts some of the symbols used
+    myparser = HTMLParser()
+    finished_string = myparser.unescape(finished_string)
+
+    return finished_string
+
+
+# Loading JSON from file and load it into a variable
+# data - untouched JSON from file
+# card_list - list of cards
+
+def loadJson(path):
+    data = urllib.request.urlopen(path)
+    content = data.read().decode('utf-8')
+    data = json.loads(content)
+    cards_list = data['cards']
+
+    return cards_list
+
+
+# This is the url from square which has the JSON object that we use
+url = 'https://fftcg.square-enix-games.com/getcards'
+
+# Load JSON
+cards = loadJson(url)
+
+# The following code is used for testing if this specific module is being executed directly and not from marcie.
+
+# Establish what our request card will be for testing from CLI passed argument
+# req = sys.argv[1]
+
+# req2 = grab_cards(req, cards)
+
+# for x in req2:
+#  print(prettyCode(x))
+
+# Print our card to CLI
+# print(grab_cards( req , cards))
+
+# print(data)
+# print(data)
